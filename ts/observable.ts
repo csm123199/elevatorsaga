a3704c2f3f59cd3dc4d34afb968bc2f81ac80e79
Chris Moore <35407569+csm123199@users.noreply.github.com>
2020-01-12 18:09:21 -0600

    remove redundant assignments in observable.ts

--- ts/observable.ts
+++ ts/observable.ts
@@ -1,63 +1,48 @@
 
 //declare const riot: typeof import('./riot_types');
 //@ts-ignore
-import * as riot from './libs/riot.es6.js'
+import { observable } from './libs/riot.es6.js'
 import { Observable as RiotObservable, ObservableCallback } from './riot_types'
 
 const LOG_EVENTS = false;
 const DEBUG_FILTER_BLACKLIST = ['stats_changed', 'stats_display_changed'];
 
 export class Observable implements RiotObservable {
-	private readonly asObservable;
-	private readonly asOriginal;
+	private readonly asObservable: RiotObservable;
 	constructor() {
-		// Backup own functions (these will get overwritten by riot.observable(this)
-		this.asOriginal = {
-			on: this.on,
-			one: this.one,
-			off: this.off,
-			trigger: this.trigger
-		};
-
-		//this.internalObservable = riot.observable(this);
-		riot.observable(this);
+		// Assign observable functions to ourselves
+		observable(this);
+		// backup observable functions to seperate method
 		this.asObservable = {
 			on: this.on,
 			one: this.one,
 			off: this.off,
-			trigger: this.trigger
-		}
-
-		// re-install own observable wrappers
-		// old ones have been overwritten by riot.observable(this)
-		
-		this.on = this.asOriginal.on;
-		this.one = this.asOriginal.one;
-		this.off = this.asOriginal.off;
-		this.trigger = this.asOriginal.trigger;
+			trigger: this.trigger,
+		};
+		// own function's are gonna be installed after the constructor
 	}
 
 	// There is no inheritable version of `observable()`.
 	on(event: string, callback: ObservableCallback): this { // look into super.on(...)
 		if(LOG_EVENTS && !DEBUG_FILTER_BLACKLIST.some(v => event == v))
 			console.log(`[DEBUG] registering event handler on ${this.constructor.name}: ${event}`);
-		return this.asObservable.on(event, callback);
+		return this.asObservable.on(event, callback) as this;
 	}
 	// Poison event handler since nothing uses it
-	one(event: never): this;
-	one(event: string, callback?: ObservableCallback): this {
-		return this.asObservable.one(event, callback);
+	one(event: never, callback: never): this;
+	one(event: string, callback: ObservableCallback): this {
+		return this.asObservable.one(event, callback) as this;
 	}
 	/** Deregister all event handlers */
 	off(event: "*"): this;
 	/** Register an event handler for `event` */
 	off(event: string, callback: ObservableCallback): this;
 	off(event: string, callback?: ObservableCallback): this {
-		return this.asObservable.off(event, callback);
+		return this.asObservable.off(event, callback) as this;
 	}
 	trigger(event: string, ...args: any[]): this {
 		if(LOG_EVENTS && !DEBUG_FILTER_BLACKLIST.some(v => event == v))
 			console.log(`[DEBUG] event triggered ${this.constructor.name}: ${event}(${args.map(v => v.toString()).join()})`);
-		return this.asObservable.trigger(event, ...args);
+		return this.asObservable.trigger(event, ...args) as this;
 	}
 }
